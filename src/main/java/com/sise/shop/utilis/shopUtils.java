af160d4617d1d9fc615e21e006083ab32ee6cfa3
Yangzh888 <1121095915@qq.com>
2019-03-08 10:18:07 +0800

    新增分页数据工具类、新增获取某月天数方法

--- src/main/java/com/sise/shop/utilis/shopUtils.java
+++ src/main/java/com/sise/shop/utilis/shopUtils.java
@@ -2,29 +2,17 @@ package com.sise.shop.utilis;
 
 import org.apache.commons.collections4.MapUtils;
 import org.thymeleaf.util.StringUtils;
+
+import java.text.Format;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * 商铺管理系统工具类
  */
 public class shopUtils {
 
-    /**
-     * 将string格式的日期转换成DATA类型
-     *
-     * @param source
-     * @return
-     */
-    public static Date formatData(String source) throws ParseException {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date date = formatter.parse(source);
-        return date;
-    }
 /**
  * 将时间格式转换成字符串
  */
@@ -59,14 +47,51 @@ public static String dateTostring(Date data){
     }
 
     /**
-     * 传入一个时间字符串+3位生成随机编号
+     * 传入一个时间字符串+5位生成随机编号
      */
     public static String getOrderNumber(String date) {
-        String data = date.replaceAll("-", "");
-        String[] split = data.split(" ");
+        String substring = date.substring(0, 10);          //获取标准值  yyyy:mm:dd
+        String date1 = substring.replaceAll("-", "");
+        String[] split = date1.split(" ");
         String orderNumberDate = split[0];
-        String random = String.valueOf((int) ((Math.random() * 9 + 1) * 100));
+        String random = String.valueOf((int) ((Math.random() * 9 + 1) * 10000));
         String orderNumber = orderNumberDate + random;
         return orderNumber;
     }
+    /**
+    *传入一个带分页数据的Map，返回一个Integer的current
+     */
+    public static Integer getCurrentByMap(Map  map) {
+        Integer current;
+        if(MapUtils.getInteger(map,"current")!=null){
+            current=MapUtils.getInteger(map,"current");
+        }else
+            current=1;
+        return current;
+    }
+
+    /**
+     * 输入一个月份，获得这个月的天数的值，返回一个Map，key是label，value是日期---用于返回图表数据
+     * @param integer
+     * @return
+     */
+    public  static Map getDaysByMonth(Integer integer,String label){
+        Map result=new HashMap();
+        if(integer==1||integer==3||integer==5||integer==7||integer==8||integer==10||integer==12){
+        for(int i=1;i<=31;i++){
+            result.put(label,i);
+        }}
+        else if(integer==4||integer==6||integer==9||integer==11){
+            for(int i=1;i<=30;i++){
+                result.put(label,i);
+        }}else {
+            if(integer!=2){
+                return result;
+            }
+           result.put(label,28);
+        }return result;
+    }
+
+
+
 }
